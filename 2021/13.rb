c0db626e7e34f41834b6cda51cd66b48923f2744
miodzie <miodzie@protonmail.com>
2021-12-13 12:08:45 -0500

    2021-13 done.

---
+++ 2021/13.rb
@@ -0,0 +1,52 @@
+input = File.read('13.in')
+
+G = []
+folds = []
+input.split("\n").each do |line|
+  x, y = line.split(',')
+  if y.nil?
+    next if x.nil?
+
+    fold = x.split(' ').dig(2).split('=')
+    fold[1] = fold[1].to_i
+    folds.push(fold)
+  else
+    G.push([x, y].map(&:to_i))
+  end
+end
+
+folds.each do |fold|
+  axis, num = fold
+  G.each_with_index do |p, i|
+    if axis == 'y'
+      G[i][1] -= (p[1] - num) * 2 if p[1] > num
+    elsif p[0] > num
+      G[i][0] -= (p[0] - num) * 2
+    end
+  end
+end
+
+G.uniq!.sort!
+graph = []
+G.each do |p|
+  graph[p[1]] = [] if graph[p[1]].nil?
+  graph[p[1]].push(p[0])
+end
+
+# y
+(0..graph.length).each do |y|
+  (0..graph[y].sort.last).each do |x|
+    if graph[y].include?(x)
+      putc '*'
+    else
+      putc ' '
+    end
+  end
+  putc "\n"
+end
+
+G.each_with_index do |row, _y|
+  row.each_with_index do |p, x|
+  end
+  # puts
+end
