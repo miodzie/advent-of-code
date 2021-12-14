b9bba4c1cf2b46e39f7811475e1f9d2ba0a0aaca
miodzie <miodzie@protonmail.com>
2021-12-14 11:54:42 -0500

    2021-14 clean up

--- 2021/14.rb
+++ 2021/14.rb
@@ -7,19 +7,14 @@ input.slice(1, input.length).map do |l|
   rules[l[0]] = l[1]
 end
 
-count = {}
-polymer.chars.each do |c|
-  count[c] = 0 unless count.has_key?(c)
-  count[c] += 1
-end
+count = Hash.new(0)
+polymer.chars.each {|c| count[c] += 1 }
 
-pairs = {}
+pairs = Hash.new(0)
 polymer.chars.each_with_index do |c, i|
   next if polymer.chars[i + 1].nil?
 
-  pair = c + polymer.chars[i + 1]
-  pairs[pair] = 0 unless pairs.has_key?(pair)
-  pairs[pair] += 1
+  pairs[c + polymer.chars[i + 1]] += 1
 end
 
 40.times do |i|
@@ -32,20 +27,12 @@ end
     # remove the pairs
     removed_pairs = pairs[rule]
     tmp[rule] -= removed_pairs
-
     # for each removed_pair, add a count for the inserted char
-    count[insert] = 0 unless count.has_key?(insert)
     count[insert] += removed_pairs
-
-    prefix = rule[0] + insert
-    # New pair is added
-    tmp[prefix] = 0 unless tmp.has_key?(prefix)
-    tmp[prefix] += removed_pairs
-
-    suffix = insert + rule[1]
-    # New pair is added
-    tmp[suffix] = 0 unless tmp.has_key?(suffix)
-    tmp[suffix] += removed_pairs
+    # prefix
+    tmp[rule[0] + insert] += removed_pairs
+    # suffix
+    tmp[insert + rule[1]] += removed_pairs
   end
   puts count.values.max - count.values.min if i == 9
   pairs = tmp.clone
