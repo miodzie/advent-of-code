1c8ab6c62019512e5a8ed010ee3742a113194cca
miodzie <miodzie@protonmail.com>
2022-12-05 15:54:54 -0500

    2022 Day 5 complete!

---
+++ 2022/day5/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"unicode"
+)
+
+type Move struct {
+	Amount, From, To int
+}
+type Crate rune
+
+func ApplyCrateMover9001Moves(moves []Move, stacks []Stack) {
+	for _, move := range moves {
+		var popped Stack
+		for i := 0; i < move.Amount; i++ {
+			popped.Push(stacks[move.From-1].Pop())
+		}
+		for i := len(popped) - 1; i >= 0; i-- {
+			stacks[move.To-1].Push(popped[i])
+		}
+	}
+}
+
+func PopOnceAll(stacks []Stack) (crates string) {
+	for _, s := range stacks {
+		if len(s) > 0 {
+			crates += string(s.Pop())
+		}
+	}
+	return crates
+}
+
+func ApplyMoves(moves []Move, stacks []Stack) {
+	for _, m := range moves {
+		stacks = ApplyMove(m, stacks)
+	}
+}
+
+func ApplyMove(move Move, stacks []Stack) []Stack {
+	for i := 0; i < move.Amount; i++ {
+		stacks[move.To-1].Push(stacks[move.From-1].Pop())
+	}
+	return stacks
+}
+
+type Stack []Crate
+
+func (s *Stack) Push(crate Crate) {
+	*s = append(*s, crate)
+}
+
+func (s *Stack) Prepend(crate Crate) {
+	*s = prepend(crate, *s)
+}
+
+func (s *Stack) Pop() Crate {
+	stack := *s
+	popped := stack[len(stack)-1]
+	*s = stack[:len(stack)-1]
+	return popped
+}
+
+func parseInput(reader io.Reader, nStacks int) ([]Stack, []Move) {
+	scanner := bufio.NewScanner(reader)
+	var moves []Move
+	stacks := make([]Stack, nStacks)
+	for scanner.Scan() {
+		// Crates
+		line := []rune(scanner.Text())
+		k := 0
+		for j := 1; j < len(line); j += 4 {
+			if unicode.IsUpper(line[j]) {
+				stacks[k].Prepend(Crate(line[j]))
+			}
+			k++
+		}
+		// Moves
+		var move Move
+		fmt.Sscanf(scanner.Text(),
+			"move %d from %d to %d",
+			&move.Amount, &move.From, &move.To)
+		if move.Amount != 0 {
+			moves = append(moves, move)
+		}
+	}
+
+	return stacks, moves
+}
+
+func prepend[T any](item T, items []T) []T {
+	return append([]T{item}, items[:]...)
+}
---
+++ 2022/day5/main_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+var sample = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+var exampleInput = strings.NewReader(sample)
+
+func TestPart1(t *testing.T) {
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	// TODO: Parse stacks automatically guh.
+	stacks, moves := parseInput(f, 9)
+	ApplyMoves(moves, stacks)
+	assert.Equal(t, "WSFTMRHPP", PopOnceAll(stacks))
+}
+
+func TestPart2(t *testing.T) {
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	// TODO: Parse stacks automatically guh.
+	stacks, moves := parseInput(f, 9)
+	ApplyCrateMover9001Moves(moves, stacks)
+	assert.Equal(t, "GSLCMFBRP", PopOnceAll(stacks))
+}
+
+func TestExampleSolutions(t *testing.T) {
+	// Part 1
+	stacks := []Stack{{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}}
+	moves := []Move{
+		{Amount: 1, From: 2, To: 1},
+		{Amount: 3, From: 1, To: 3},
+		{Amount: 2, From: 2, To: 1},
+		{Amount: 1, From: 1, To: 2},
+	}
+	ApplyMoves(moves, stacks)
+	assert.Equal(t, "CMZ", PopOnceAll(stacks))
+
+	// Part 2
+	stacks = []Stack{{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}}
+	ApplyCrateMover9001Moves(moves, stacks)
+	assert.Equal(t, "MCD", PopOnceAll(stacks))
+}
+
+func TestApplyMove(t *testing.T) {
+	stacks := []Stack{{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}}
+	move := Move{Amount: 1, From: 2, To: 1}
+	expected := []Stack{{'Z', 'N', 'D'}, {'M', 'C'}, {'P'}}
+
+	assert.Equal(t, expected, ApplyMove(move, stacks))
+}
+
+func TestApplyCrateMover9001Moves(t *testing.T) {
+	stacks := []Stack{{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}}
+	moves := []Move{{Amount: 1, From: 2, To: 1}, {Amount: 3, From: 1, To: 3}}
+	expected := []Stack{{}, {'M', 'C'}, {'P', 'Z', 'N', 'D'}}
+	ApplyCrateMover9001Moves(moves, stacks)
+	assert.Equal(t, expected, stacks)
+}
+
+func TestApplyCrateMover9001Moves2(t *testing.T) {
+	stacks := []Stack{{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}}
+	moves := []Move{{Amount: 1, From: 2, To: 1},
+		{Amount: 3, From: 1, To: 3},
+		{Amount: 2, From: 2, To: 1}}
+	expected := []Stack{{'M', 'C'}, {}, {'P', 'Z', 'N', 'D'}}
+	ApplyCrateMover9001Moves(moves, stacks)
+	assert.Equal(t, expected, stacks)
+}
+
+func TestParseInput(t *testing.T) {
+	stacks, moves := parseInput(exampleInput, 3)
+	expected := []Stack{
+		{'Z', 'N'},
+		{'M', 'C', 'D'},
+		{'P'},
+	}
+	expectedMoves := []Move{
+		{Amount: 1, From: 2, To: 1},
+		{Amount: 3, From: 1, To: 3},
+		{Amount: 2, From: 2, To: 1},
+		{Amount: 1, From: 1, To: 2},
+	}
+
+	assert.Equal(t, expected, stacks)
+	assert.Equal(t, expectedMoves, moves)
+	assert.Len(t, stacks, 3)
+}
