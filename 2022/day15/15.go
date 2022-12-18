14cec05cc2ab9fb1f8d9fb57fea498c2b0ec44b4
miodzie <miodzie@protonmail.com>
2022-12-18 16:42:39 -0500

    Day15 not done, trying out C#

--- 2022/day15/15.go
+++ 2022/day15/15.go
@@ -1,1 +1,82 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/miodzie/advent-of-code/util"
+	"io"
+)
+
+func makeTunnel(r io.Reader) Tunnels {
+	s, b := ParseInput(r)
+	tunnels := make(Tunnels)
+	tunnels.AddCoords(s)
+	tunnels.AddCoords(b)
+	return tunnels
+}
+
+func NonBeaconsAtY(tunnels []Coord, y int) (sum int) {
+	var xMin, xMax int
+	atY := make(map[Coord]bool)
+	for _, t := range tunnels {
+		if t.X <= xMin {
+			xMin = t.X
+		}
+		if t.X >= xMax {
+			xMax = t.X
+		}
+		if t.Y == y && t.Type == Beacon {
+			atY[t] = true
+		}
+	}
+	if xMin < 0 {
+		xMin *= -1
+	}
+	distance := xMax + xMin
+	fmt.Println(xMax, xMin, atY, len(atY))
+
+	return distance - len(atY)
+}
+
+type Tunnels map[Coord]Type
+
+func (t Tunnels) AddCoords(coords []Coord) {
+	for _, c := range coords {
+		t[c] = c.Type
+	}
+}
+
+type Type int
+
+const (
+	Air Type = iota
+	Sensor
+	Beacon
+)
+
+type Coord struct {
+	X, Y int
+	Type Type
+}
+
+func (c Coord) TaxicabDistance(b Coord) Coord {
+	return Coord{X: util.Abs(c.X - b.X), Y: util.Abs(c.Y - b.Y)}
+}
+
+func ParseInput(reader io.Reader) (sensors []Coord, beacons []Coord) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		s, b := parseLine(scanner.Text())
+		sensors = append(sensors, s)
+		beacons = append(beacons, b)
+	}
+	return
+}
+
+func parseLine(line string) (sensor Coord, beacon Coord) {
+	sensor.Type = Sensor
+	beacon.Type = Beacon
+	fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d",
+		&sensor.X, &sensor.Y, &beacon.X, &beacon.Y)
+	return sensor, beacon
+}
--- 2022/day15/15_test.go
+++ 2022/day15/15_test.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
-func TestNothing(t *testing.T) {
+func TestCoord_TaxicabDistance(t *testing.T) {
+	s := Coord{2, 18, Sensor}
+	b := Coord{-2, 15, Beacon}
+	assert.Equal(t, Coord{X: 4, Y: 3}, s.TaxicabDistance(b))
+}
 
+func TestSolution1(t *testing.T) {
+	f, _ := os.Open("input")
+	s, b := ParseInput(f)
+	s = append(s, b...)
+	assert.Equal(t, 26, NonBeaconsAtY(s, 2_000_000))
+	// 4893541 is too low.
+	// 4893537 is too low
+	// 4893541
+}
+
+func TestNumNonBeaconsAtY_Example1(t *testing.T) {
+	example, _ := os.Open("example")
+	s, b := ParseInput(example)
+	s = append(s, b...)
+	assert.Equal(t, 26, NonBeaconsAtY(s, 10))
+}
+
+func TestParseInput(t *testing.T) {
+	f, _ := os.Open("example")
+	sensors, beacons := ParseInput(f)
+	assert.Equal(t, Coord{2, 18, Sensor}, sensors[0])
+	assert.Equal(t, Coord{-2, 15, Beacon}, beacons[0])
+	assert.Len(t, sensors, 14)
+	assert.Len(t, beacons, 14)
+}
+
+func TestParseLine(t *testing.T) {
+	l := "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+	sensor, beacon := parseLine(l)
+	assert.Equal(t, Coord{2, 18, Sensor}, sensor)
+	assert.Equal(t, Coord{-2, 15, Beacon}, beacon)
 }
